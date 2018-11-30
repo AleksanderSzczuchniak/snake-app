ab23701f45333711e3152eef827a47a1dcda9a35
AleksanderSzczuchniak <alex.szczuchniak@gmail.com>
2018-11-30 10:13:46 +0100

    GameBoard, GameRow, GameCell

---
+++ src/Snake/GameBoard.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import GameRow from './GameRow'
+
+const style = {
+    width: '100%',
+    maxWidth: '700px',
+    margin: '0 auto'
+}
+
+const GameBoard = (props) => (
+    <div
+    style={style}
+    >
+        {
+            props.gameBoard.map(row => (
+                <div>
+                    <GameRow
+                        row={row}
+                    />
+                </div>
+            ))
+        }
+    </div>
+)
+export default GameBoard
