966aec8ade917544d44d4f1d410e4f6c396aec5b
AleksanderSzczuchniak <alex.szczuchniak@gmail.com>
2018-11-30 14:56:34 +0100

    firebase connection

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -11,6 +11,4 @@ const config = {
 
 firebase.initializeApp(config)
 
-export const auth = firebase.auth()
 export const database = firebase.database()
-export const googleProvider = new firebase.auth.GoogleAuthProvider()
